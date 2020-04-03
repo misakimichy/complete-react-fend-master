5f9f0eb55ea8e4c4f35bf68bd5a8047f0e5ac4f4
Misaki Koonce <misaki.koonce@gmail.com>
2020-04-02 22:03:52 -0700

    Install Parcel and bind the basic react files.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Pet from './Pet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Coop",
+            animal: "Cat",
+            breed: "British Shorthair"
+        }),
+        React.createElement(Pet, {
+            name: "Cosmo",
+            animal: "BirDogd",
+            breed: "Corgi"
+        }),
+    React.createElement(Pet, {
+            name: "Mocha",
+            animal: "Cat",
+            breed: "British Shorthair"
+    })
+    ]);
+  };
+  ReactDOM.render(React.createElement(App), document.getElementById("root"));
