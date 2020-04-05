f46c0707d70bc3fe05b249bc6dff26afdcb0952a
Misaki Koonce <misaki.koonce@gmail.com>
2020-04-04 22:12:32 -0700

    Render as JSX.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { render } from 'react-dom'
 import Pet from './Pet'
 
 const App = () => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-    ]);
-  };
-  render(React.createElement(App), document.getElementById("root"));
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name='Coop' animal="Cat" breed="British Shorthair" />
+      <Pet name='Tots' animal="Cat" breed="British Shorthair" />
+      <Pet name='Cosmo' animal="Dog" breed="Corgi" />
+    </div>
+  );
+};
+
+render(React.createElement(App), document.getElementById("root"));
